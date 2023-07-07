7eb1fb19362feaba3e096d147102e5671bea813f
Brian Ho <bh1525@nyu.edu>
2023-07-07 15:10:35 -0400

    removed controls

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -59,7 +59,7 @@ export default function Layout({ children }) {
 				{/* <SoftShadows /> */}
 				<Environment files='https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/industrial_workshop_foundry_1k.hdr' />
 				{children}
-				<Controls />
+				{/* <Controls /> */}
 			</Canvas>
 		</div>
 	)
