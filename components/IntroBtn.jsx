4873efb2a27d9a91b5adccecbd4a31d1a326c0e9
Brian Ho <bh1525@nyu.edu>
2023-07-10 20:58:39 -0400

    fixed button size

--- components/IntroBtn.jsx
+++ components/IntroBtn.jsx
@@ -5,7 +5,7 @@ const IntroBtn = (props) => {
 		<>
 			<group position={props.position}>
 				<RoundedBox
-					args={[0.55, 0.25, 0.001]} // Width, height, depth. Default is [1, 1, 1]
+					args={[0.43, 0.15, 0.001]} // Width, height, depth. Default is [1, 1, 1]
 					radius={0.05} // Radius of the rounded corners. Default is 0.05
 					smoothness={4} // The number of curve segments. Default is 4
 					creaseAngle={0.4} // Smooth normals everywhere except faces that meet at an angle greater than the crease angle
@@ -15,7 +15,7 @@ const IntroBtn = (props) => {
 				</RoundedBox>
 				<Title
 					text='Enter'
-					scale={[0.1, 0.1, 0.1]}
+					scale={[0.075, 0.075, 0.075]}
 					col='Black'
 					position={[0, 0, 0.001]}
 				/>
