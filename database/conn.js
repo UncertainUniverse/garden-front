6500ff2b6817abc55e9d58f93a4048d594db5104
Brian Ho <bh1525@nyu.edu>
2023-07-17 12:19:35 -0400

    get data from mongo and save as local

--- database/conn.js
+++ database/conn.js
@@ -1,45 +1,55 @@
 // Run `npm run db` to retrieve all the documents on the command line.
+const fs = require('fs')
+const mongoose = require('mongoose')
+const { MongoClient, ServerApiVersion } = require('mongodb')
 
-const mongoose = require('mongoose');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const collections = ["first-principles", "introduction"];
-const db_uri = "mongodb+srv://admin:srPiJzF8MVXkkqpL@sample-db.tkcfqem.mongodb.net/?retryWrites=true&w=majority";
+const collections = ['first-principles', 'introduction']
+const db_uri =
+	'mongodb+srv://admin:srPiJzF8MVXkkqpL@sample-db.tkcfqem.mongodb.net/?retryWrites=true&w=majority'
 
 const client = new MongoClient(db_uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+})
 
 // We currently have two collections populated: `first-principles` and `introduction`
-async function retrievePosts(branch) {
-    if (!collections.includes(branch)) {
-        console.error("Cannot retrieve data from invalid collection: ", branch);
-    }
+async function retrievePosts(client, branch) {
+	if (!collections.includes(branch)) {
+		console.error('Cannot retrieve data from invalid collection: ', branch)
+		return
+	}
 
-    try {
-        await client.connect();
-        const db = client.db("uncertain-universe");
-        const collection = db.collection(branch);
+	try {
+		const db = client.db('uncertain-universe')
+		const collection = db.collection(branch)
 
-        // To get all the posts from a collection; {} is an empty filter
-        const allPosts = collection.find({});
+		const allPosts = await collection.find({}).toArray()
 
-        await allPosts.forEach(console.log);
-    } catch (e) {
-        console.error(e);
-    }
+		const allPostsJson = JSON.stringify(allPosts, null, 2)
+		fs.writeFileSync(`./${branch}.json`, allPostsJson, 'utf8')
+
+		console.log(`${branch}.json file has been saved`)
+	} catch (e) {
+		console.error(e)
+	}
 }
 
 async function main() {
-    collections.forEach(col => {
-        retrievePosts(col);
-    });
+	try {
+		await client.connect()
+		await Promise.all(
+			collections.map((col) => {
+				return retrievePosts(client, col)
+			})
+		)
+	} catch (error) {
+		console.error(error)
+	} finally {
+		await client.close()
+	}
 }
 
-main().then(console.log).catch(console.error).finally(() => {
-    client.close();
-});
+main().catch(console.error)
